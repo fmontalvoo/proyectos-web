10b7d16319096d59dd4079a5d84a2bbb88737d89
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-06-12 22:04:03 -0500

    I18n pt2.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- i18n/assets/js/i18.js
+++ i18n/assets/js/i18.js
@@ -1,16 +1,35 @@
-async function loadLocale() {
-    const locale = navigator.language || 'en';
-    const file = await fetch(`./assets/locale/locale-${locale.split('-')[0]}.json`);
+async function loadLocale(locale = 'es') {
+    // const locale = navigator.language;
+    const file = await fetch(`./assets/locale/locale-${locale}.json`);
     return await file.json();
 
 }
 
-document.addEventListener("DOMContentLoaded", async function (event) {
-    const lang = await loadLocale();
+async function changeLanguage(locale) {
+    const lang = await loadLocale(locale);
 
     const pageTitle = document.getElementById('page_title');
     pageTitle.innerText = lang.page_title;
 
+    const btnEs = document.getElementById('btn-es');
+    btnEs.innerText = lang.btn_es;
+    const btnEn = document.getElementById('btn-en');
+    btnEn.innerText = lang.btn_en;
+
+    btnEs.addEventListener('click', () => changeLanguage('es'));
+    btnEn.addEventListener('click', () => changeLanguage('en'));
+
     const title = document.getElementById('title');
     title.innerText = lang.title;
+
+    const content = document.getElementById('content');
+    content.innerText = '';
+    for (const line of lang.content) {
+        content.innerText += line;
+    }
+}
+
+// Carga el lenguaje cuando la pagina termina de cargar.
+document.addEventListener("DOMContentLoaded", function (event) {
+    changeLanguage();
 });
